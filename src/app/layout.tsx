134e5feb7726722268fc63d7f85478d86a5dc87e
Machoudi2002 <stehat982@gmail.com>
2024-01-08 23:53:18 +0100

    Add : Apply Tailwind Styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Noto_Serif } from 'next/font/google'
 import './globals.css'
+import 'tailwindcss/tailwind.css';
 import Header from '@/Components/Header'
 import Footer from '@/Components/Footer'
 
